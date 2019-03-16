452a423ae527cb1795c05573e52853939b48638c
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-16 23:16:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 def line(katz_deli)
-    new_array = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    new_array.each.with_index(0) do |name,index|
-      new_array.push("#{index}. #{name}")
+    message="The line is currently:"
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
     end
-    puts "The line is currently: #{new_array.join(" ")}"
-  end
+    puts "#{message}"
 end
