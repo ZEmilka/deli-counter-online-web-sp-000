13398dc4f822618b6c5d24b01498c7dcd2160fb7
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-16 23:08:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 def line(katz_deli)
   if katz_deli.length >= 1
     new_array = []
-    number = 1 
+    number = 1
     katz_deli.each do |name|
       new_array.push("#{number}. #{name}")
-      counter += 1 
-    end 
+      counter += 1
+    end
     puts "The line is currently: #{new_array.join(" ")}"
   else
     puts "The line is currently empty."
